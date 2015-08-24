c38490d92f0352c0aa74692ffa0c6c795d7ccd46
kcn1.71 <kcn1.71@gmail.com>
2015-08-24 16:52:30 +0600

    4

---
+++ Lerning(.01)/NewLearning(V0.1)/Models/Homework.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewLearning_V0._1_.Models
+{
+    public class Homework
+    {
+        public int HomeworkId { get; set; }
+        public string HomeworkText { get; set; }
+        public DateTime HomeworkDate { get; set; }
+    }
+}
