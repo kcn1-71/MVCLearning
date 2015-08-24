5e3c6da74f86a412f34dc57b2e06c4ebafe5b49f
kcn1.71 <kcn1.71@gmail.com>
2015-08-24 16:45:21 +0600

    3

---
+++ NewLearning(V0.1)/NewLearning(V0.1)/Models/HomeworkContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NewLearning_V0._1_.Models
+{
+    public class HomeworkContext : DbContext
+    {
+        public DbSet<Homework> HomeworkList { get; set; }
+    }
+}
---
+++ NewLearning(V0.1)/NewLearning(V0.1)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewLearning_V0._1_.Startup))]
+namespace NewLearning_V0._1_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
